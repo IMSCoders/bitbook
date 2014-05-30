6bb5d5f5a28ab5539f2a1b24a6d488987ff84b8e
Amit Seal Ami <lordamit@gmail.com>
2014-05-30 11:18:44 +0600

    primary setup of BitBookHub

---
+++ BitBookHub/Bitbookhub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace BitBookHub
+{
+    [HubName("BitBookHub")]
+    public class Bitbookhub : Hub
+    {
+        [HubMethodName("")]
+        public void Send(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+
+        }
+
+        [HubMethodName("IncludeTime")]
+        public void IncludeTime(string time)
+        {
+            //Clients.All.broadcastTime(time);
+            Clients.All.broadCastTime(time);
+
+        }
+    }
+}
---
+++ BitBookHub/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(BitBookHub.Startup))]
+
+namespace BitBookHub
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HubConfiguration config = new HubConfiguration();
+            config.EnableJavaScriptProxies = true;
+            config.EnableDetailedErrors = true;
+            config.EnableJSONP = true;
+            app.MapSignalR(config);
+        }
+    }
+}
