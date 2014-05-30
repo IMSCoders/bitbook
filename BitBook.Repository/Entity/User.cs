a702545a52102880fbe2b428334ce2073178558d
MIbnoon <mibnoon@bd.imshealth.com>
2014-05-30 11:10:48 +0600

    Repository Created

---
+++ BitBook.Repository/Entity/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace BitBook.Repository.Entity
+{
+    class User
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhotoPath { get; set; }
+        public List<ObjectId> Friends { get; set; }
+    }
+}
